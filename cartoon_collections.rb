53e838e7d9e8af49f788f229a3dd3453201a749c
Sandie Nunez <snunez@vt.edu>
2020-04-04 20:55:37 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,8 +21,8 @@ end
 
 
 
-def find_the_cheese2(array, array2)
-  cheese_types = array2
+def find_the_cheese(array)
+  cheese_types = ["cheddar", "gouda", "camembert"]
     array.find do |type|
     cheese_types.include?(type)
   end 
