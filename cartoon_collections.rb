ea768ea232e16f32cac569925685de632315b81f
Sandie Nunez <snunez@vt.edu>
2020-04-04 15:22:37 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,6 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(names)
+  each_with_index.collect
+  
 end
 
 def summon_captain_planet# code an argument here
